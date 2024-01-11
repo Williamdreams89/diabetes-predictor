b5a44b82d0c5574fdabcecccb25bec3c01202279
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-01-11 07:54:22 +0000

    clean code

--- api/model/main.py
+++ api/model/main.py
@@ -18,23 +18,6 @@ with open(f"{BASE_DIR}/diabetes_pred_model-{__version__}.pkl", "rb") as f:
     model = pickle.load(f)
 
 
-features = ['pregnant', 'glucose', 'pressure', 'triceps', 'insulin', 'mass',
-       'pedigree', 'age', 'diabetes']
-
-def prediction(input_data):
-    scaler = StandardScaler()
-    # Convert the input data as np array
-    input_data_array = np.asarray(input_data)
-
-    # Reshape the data since we are only predicting for a single instance
-    reshaped_input_data = input_data_array.reshape(1, -1)
-
-    # transform data into std data
-    std_data = scaler.transform(reshaped_input_data)
-
-    # Now make the prediction
-    return model.predict(std_data)
-
 app = FastAPI()
 
 
